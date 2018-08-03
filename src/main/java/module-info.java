58c6bb677e6a192dd58dae6b307dcf122b1c5b51
Konstantin Bakaras <kbakaras@gmail.com>
2018-08-03 21:04:48 +0300

    Начал готовить бибилотеку к модульности.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module sugar {
+    exports ru.kbakaras.sugar.compare;
+    exports ru.kbakaras.sugar.dates;
+    exports ru.kbakaras.sugar.dimensional;
+    exports ru.kbakaras.sugar.entity;
+    exports ru.kbakaras.sugar.function;
+    exports ru.kbakaras.sugar.lazy;
+    exports ru.kbakaras.sugar.listeners;
+    exports ru.kbakaras.sugar.tree;
+    exports ru.kbakaras.sugar.tree.annotations;
+    exports ru.kbakaras.sugar.utils;
+}
